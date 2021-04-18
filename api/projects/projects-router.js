f6843d86fa97d3b3ebdc9977d781cb3a1d87ef2b
Jakobi Matagi <jkmatagi@hotmail.com>
2021-04-18 17:42:10 -0600

    check

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,18 @@
 // Write your "projects" router here!
+const express = require('express')
+
+const router = express.Router();
+
+const projects = require('./projects-model')
+
+router.get('/', (req, res, next)=>{
+    projects.get()
+    .then((projects)=>{
+        res.status(200).json(projects)
+    })
+    .catch(
+        (err)=>{
+            next(err)
+        }
+    )
+})
--- api/server.js
+++ api/server.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const server = express();
-
+const projectsRouter = require('./projects/projects-router')
+const actionsRoter = require('./actions/actions-router')
 // Complete your server here!
+server.use(express.json())
+server.use('/projects', projectsRouter)
+server.use('/actions', actionsRoter)
 // Do NOT `server.listen()` inside this file!
 
+
+server.use((err, req, res)=>{
+    console.log(err)
+  
+    res.status(500).json({
+      message: "Something went wrong"
+    })
+  })
 module.exports = server;
